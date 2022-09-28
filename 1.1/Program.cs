49e5ebd5eec757ee6757ecc576995c44c0d3aa10
kchau <90921523+cybkate@users.noreply.github.com>
2022-09-28 19:35:08 +0300

    oop1.1

---
+++ 1.1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введіть число!");
+var n = Console.ReadLine();
+try
+{
+    int x = int.Parse(n);
+    Console.WriteLine($"Ви ввели число {x}");
+}
+catch
+{
+    Console.WriteLine("Ви не ввели число");
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,23 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int a = int.Parse(textBox1.Text);
+                MessageBox.Show($"Ви ввели  число {a}");
+            }
+            catch
+            {
+                MessageBox.Show("Ви не ввели  число");
+            }
+        }
+    }
+}
